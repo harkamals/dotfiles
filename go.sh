1e7a09f88c666a94628613e4d3b66714766cc433
Harkamal Singh <harkamals@gmail.com>
2018-05-31 23:40:21 +0800

    updated go

--- go.sh
+++ go.sh
@@ -4,3 +4,13 @@ export GOBIN=/Users/hk/go/bin
 export PATH=$PATH:$GOPATH/bin
 
 alias gov='go version'
+
+# Dependency manager
+go get -u -v github.com/golang/dep/cmd/dep
+
+# Essentials
+go get -u -v github.com/aws/aws-sdk-go
+go get -u -v github.com/hashicorp/hcl
+go get -u -v github.com/hashicorp/terraform
+go get -u -v github.com/astaxie/beego
+go get -u -v github.com/beego/bee
